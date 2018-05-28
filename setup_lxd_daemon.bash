2d973d12dfbd88277705ffa763da9b902f52e9f3
Helmut Schmidt <noreply@helmut.schmidt>
2018-05-28 23:57:58 +0200

    added bash script for creating and running lxd daemon

---
+++ setup_lxd_daemon.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+service=/etc/systemd/system/lxd.service
+
+cat > $service <<EOF
+[Unit]
+Description=LXD daemon
+After=network.target
+
+[Service]
+Type=simple
+Restart=always
+ExecStart=/usr/local/bin/lxd --group sudo
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl daemon-reload
+systemctl enable lxd.service
+systemctl start lxd.service
